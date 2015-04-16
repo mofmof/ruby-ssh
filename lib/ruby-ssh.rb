0509e0eebedf341b00ec6bf54fba1eab9c0397c4
a.harada <redhornet96@gmail.com>
2015-04-16 14:14:58 +0900

    change #start interface, add #run oneliner execute script on ssh

--- lib/ruby-ssh.rb
+++ lib/ruby-ssh.rb
@@ -4,13 +4,24 @@ require "ruby-ssh/session"
 require "ruby-ssh/shell_runner"
 require "ruby-ssh/result"
 
-=begin
 module RubySSH
-  def self.start(host, user, options = {}, &block)
-    Net::SSH::start(host, user, options) do |ssh|
-      p ssh.exec!('ls ~')
-      #yield
+  class << self
+    def start(host, user, options = {}, &block)
+      Net::SSH.start(host, user, options) do |ssh|
+        yield ssh
+      end
+    end
+
+    def run(host, user, options = {})
+      script = options.delete(:script)
+
+      result = nil  # TODO 明示的に初期化したくない
+      Net::SSH.start(host, user, options) do |ssh|
+        ssh.shell_runner do |runner|
+          result = runner.exec(script)
+        end
+      end
+      result
     end
   end
 end
-=end
--- spec/ruby-ssh_spec.rb
+++ spec/ruby-ssh_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 # TODO sshアクセスするようなところのrspecはどうしたらいいんだろう net-ssh-shellを参考にしてみる
 describe RubySSH do
   describe '' do
-    it do
+    skip it do
       # TODO hard coding
       Net::SSH.start('192.168.33.11', 'root', password: 'testpass') do |ssh|
         ssh.shell_runner do |runner|
@@ -14,6 +14,24 @@ describe RubySSH do
           expect(true).to be_truthy
         end
       end
+    end
+
+    it do
+      # TODO hard coding
+      RubySSH.start('192.168.33.11', 'root', password: 'testpass') do |ssh|
+        ssh.shell_runner do |runner|
+          result = runner.exec('ls -l')
+          puts result.stdout
+          puts result.exit_status
+          puts result.success?
+          expect(true).to be_truthy
+        end
+      end
+    end
+
+    skip it do
+      result = RubySSH.run('192.168.33.11', 'root', password: 'testpass', script: 'ls')
+      expect(result.success?).to be_truthy
     end
   end
 end
